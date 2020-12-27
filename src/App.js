91b7416a75a97230ad84a75ad8b66dfd63dac34e
ssexton1@uw.edu <ssexton1@uw.edu>
2020-12-26 16:33:19 -0800

    initial start css and html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tasks from "./Tasks";
+// import { Route, Switch, Redirect} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header> 
+        <h1 className="pageTitle">Productivity Pal</h1>
       </header>
+
+
+      <Tasks />
+      {/* <Switch>
+        <Route exact path="/" render={Map} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route path="/report" component={Form} />
+        <Route path="/map/:raceName"></Route>
+        <Route exact path="/table" component={Table} />
+        <Route exact path="/table/:personName" component={PersonProfile} />
+        <Redirect to="/" />
+      </Switch> */}
+
+      <footer>
+        {/* <address>
+          Contact us at <a href="Email@Gmail.com">Email@Gmail.com</a>, or at <a href="tel:866-588-0569">Telephone Number</a>.
+        </address> */}
+        <p>&copy; 2020</p>
+      </footer>
     </div>
   );
 }
---
+++ src/Tasks.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Tasks() {
+  return (
+    <div className="flex-container">
+      <p className="task">Task 1</p>
+      <p className="task">Task 2</p>
+      <p className="task">Task 3</p>
+      <p className="task">Task 4</p>
+      <p className="task">Task 5</p>
+    </div>
+  );
+}
+
+export default Tasks;
